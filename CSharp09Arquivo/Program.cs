a5d3e4c7630d9069cfbe5b712e690e2f3aec90eb
Francisco Lucas Sens <franciscosensaulas@gmail.com>
2023-11-26 18:22:05 -0300

    Finalizado exemplo de leitura e escrita manual em arquivo CSV

--- CSharp09Arquivo/Program.cs
+++ CSharp09Arquivo/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Caminho do arquivo de CSV que será armazenado o nome das pessoas com sua idade
+using ConsoleTables;
+
 string caminho = "C:\\Users\\moc\\Desktop\\arquivo.csv";
 
 // Verificar que o arquivo CSV não existe
@@ -10,22 +12,45 @@ if (File.Exists(caminho) == false)
 }
 else
 {
-    string registrosTexto = File.ReadAllText(caminho);
-    // Quebrar a string em um vetor de strings
-    string[] registros = registrosTexto.Split("\n");
+    // Ler o conteúdo do arquivo retornando um vetor de linhas
+    string[] registros = File.ReadAllLines(caminho);
+    // Para obter a quantidade de posições do vetor utilizamos a propriedade .Length
+    int quantidadeRegistros = registros.Length;
 
-    Console.WriteLine(registros[0]);
-    Console.WriteLine(registros[1]);
-    Console.WriteLine("\n\n\n");
-    //[0] = "Nome;Idade"
-    //[1] = "Francisco;20"
-    //[2] = "Julio;40"
+    Console.WriteLine("Registros do arquivo.csv");
 
+    ConsoleTable tabela = new ConsoleTable("Nome", "Idade");
 
+    int indice = 0;
+    // Percorrer cada um dos registros 
+    while (indice < quantidadeRegistros)
+    {
+        // Se for a linha do cabeçalho, não apresentar para o usuário
+        if (indice == 0)
+        {
+            indice = indice + 1;
+            // Continue é um comando ignorar a execução do conteúdo abaixo do continue
+            continue;
+        }
+
+        // Obter a linha (registro)
+        string registro = registros[indice];
+        // Quebrar o registro "Francisco;20" em um vetor, para poder extrair o nome e a idade
+        string[] colunas = registro.Split(";");
+        // Obter o nome que está na primeira(0) posição do vetor de colunas
+        string nome = colunas[0];
+        // Obter a idade que está na segunda(1) posição do vetor de colunas
+        int idade = Convert.ToInt32(colunas[1]);
+        tabela.AddRow(nome, idade);
+
+        indice = indice + 1;
+    }
+    // Apresentar a tabela
+    tabela.Write();
 }
 
 // Questiona para o usuário se ele deseja cadastrar registro
-Console.Write("Deseja cadastrar algum registro? [S/N] ");
+Console.Write("\n\nDeseja cadastrar algum registro? [S/N] ");
 string desejaContinuar = Console.ReadLine().Trim().ToLower();
 
 while (desejaContinuar == "s")
